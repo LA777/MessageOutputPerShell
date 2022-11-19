29ade49faca974958ec63bedd13cf5928b88ea3b
Alex L <LA.xdna@gmail.com>
2022-11-19 22:29:57 +0200

    initial commit

---
+++ mops/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Forms;
+
+namespace mops
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var message = string.Empty;
+            var caption = string.Empty;
+            foreach (var arg in args)
+            {
+                var split = arg.Split(':');
+
+                if (split[0].StartsWith("-message"))
+                {
+                    message = split[1];
+                }
+                else if (split[0].StartsWith("-caption"))
+                {
+                    caption = split[1];
+                }
+            }
+
+            MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Asterisk, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+        }
+    }
+}
